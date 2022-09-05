ff31eb02c84874d766c70a20e6541580ff743d29
Michael Rowe <roweboat87@gmail.com>
2022-09-04 19:09:45 -0500

    Added new functions

--- python_backend/si4703Example.py
+++ python_backend/si4703Example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python 2
 
-#example program for testing the si4703 library
+# example program for testing the si4703 library
 from si4703Library import si4703Radio
 import zmq
 
@@ -19,7 +19,7 @@ def main():
 
     try:
         while True:
-            #check for stuff
+            # check for stuff
             message = socket.recv_string()
             print("received request: %s" % message)
 
@@ -33,27 +33,28 @@ def main():
                 radio.si4703SetChannel(1003)
                 socket.send_string(str(radio.si4703GetChannel()))
             if message == "+":
-                radio.si4703SetVolume(radio.si4703GetVolume()+1)
+                radio.si4703SetVolume(radio.si4703GetVolume() + 1)
                 socket.send_string(str(radio.si4703GetVolume()))
             if message == "-":
-                radio.si4703SetVolume(radio.si4703GetVolume()-1)
+                radio.si4703SetVolume(radio.si4703GetVolume() - 1)
                 socket.send_string(str(radio.si4703GetVolume()))
             if message == "d":
                 radio.si4703ClearRDSBuffers()
-                test = radio.si4703ReadRDS
-                print(test)
-                socket.send_string(str(test))
+                radio.si4703GetRDSData()
+                # print(test)
+                # socket.send_string(str(test))
             if message == "t":
                 print("connected to Iphone")
             if message == "r":
                 break
-            
+
     except KeyboardInterrupt:
         socket.send_string("Exiting program")
-        
+
     socket.send_string("Shutting down radio")
     radio.si4703ShutDown()
     socket.send_string("Radio has been turned off")
-    
+
+
 if __name__ == "__main__":
     main()
